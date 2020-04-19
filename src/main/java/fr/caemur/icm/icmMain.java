aa14e9b55c188b7282ec34e2beaa9f57e84eb197
GD-1z2 <matthieu.schaerlinger@gmail.com>
2020-04-19 11:24:42 +0200

    Super commit message

---
+++ src/main/java/fr/caemur/icm/icmMain.java
@@ -0,0 +1,9 @@
+package fr.caemur.icm;
+
+import fr.caemur.icm.utils.References;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = References.MODID, name = References.NAME, version = References.VERSION)
+public class icmMain {
+	
+}
---
+++ src/main/java/fr/caemur/icm/utils/References.java
@@ -0,0 +1,7 @@
+package fr.caemur.icm.utils;
+
+public class References {
+	public static final String MODID = "icm";
+	public static final String NAME = "Caemur's industrial mod";
+	public static final String VERSION = "1.0";
+}
